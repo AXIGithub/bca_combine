367549ddc3fc3bfbcfd5dc14060506caa6b4f20d
ratinoAG14 <ratino@astragraphia.co.id>
2022-05-19 09:35:16 +0700

    Inisialisasi git

---
+++ src/bca_combine/BCA_COMBINE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bca_combine;
+
+/**
+ *
+ * @author Ratino
+ */
+public class BCA_COMBINE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
